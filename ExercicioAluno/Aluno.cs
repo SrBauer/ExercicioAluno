0f349a3a49b360fc4003d2c8b7156d59a8d79c76
SrBauer <bauer.jonathan.jj@gmail.com>
2024-04-02 16:55:40 -0300

    first commit

---
+++ ExercicioAluno/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAluno
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double nota1, nota2, nota3;
+
+        public double NotaFinal()
+        {
+            return (nota1 + nota2 + nota3) / 3;
+        }
+        public bool Aprovado()
+        {
+            if (NotaFinal() > 5.99){
+                return true;
+            }
+            else { return false; }
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 6.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ ExercicioAluno/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExercicioAluno;
+using System;
+using System.Globalization;
+
+namespace ExercioAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Informe o nome do Aluno: ");
+            aluno.nome = Console.ReadLine();
+            Console.WriteLine("Informe as 3 notas do aluno: ");
+            aluno.nota1 = double.Parse(Console.ReadLine());
+            aluno.nota2 = double.Parse(Console.ReadLine());
+            aluno.nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota Final: " + aluno.NotaFinal().ToString("F2"));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO!");
+                Console.WriteLine("Faltou: " + aluno.NotaRestante().ToString("F2"));
+            }
+        }
+    }
+}
